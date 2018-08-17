a8084892bc5986043bf7718132da451f6a970838
jzhw0130 <569274132@qq.com>
2018-08-17 13:59:59 +0800

    Add prettier

--- playground/promise.js
+++ playground/promise.js
@@ -1,18 +1,21 @@
 import axios from 'axios';
-// const axios = require('axios');
 
 const HOST = 'api.dev.ihealth-care.com/v1';
 
-axios.get(`http://${HOST}/common/countries`).then(respone => {
-    console.log('1', respone.status)
-    console.log('2', respone.data);
-}).catch(e => {
-    console.log('e', e);
-})
+axios
+   .get(`http://${HOST}/common/countries`)
+   .then(respone => {
+      console.log('1', respone.status);
+      console.log('2', respone.data);
+   })
+   .catch(e => {
+      console.log('e', e);
+   });
 
+const fetchRaces = async () => {
+   return await axios.get(`http://${HOST}/common/races`);
+};
 
-
-
-
-
-
+fetchRaces()
+   .then(response => console.log('response', response.data))
+   .catch(e => console.log('e', e));
